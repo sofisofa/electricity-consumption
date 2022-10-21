c8d9af98786d63b2feedf901aba9ccbde0bbd607
sofia <106708718+sofisofa@users.noreply.github.com>
2022-10-21 18:19:07 +0200

    Added tests for login

--- test_for_holaluz.py
+++ test_for_holaluz.py
@@ -5,15 +5,33 @@ from holaluz_api import HolaLuz
 
 class HolaluzTestCase(unittest.TestCase):
     
-    # @responses.activate
-    # def test_holaluz_login(self):
-        
-    #     resp = responses.post("https://example.com", "status" : 200, json = {"token": "token", "refresh_token" : "refresh_token"})
+    @responses.activate
+    def test_holaluz_login(self):
+        # Given
+        responses.post(
+            "https://core.holaluz.com/api/private/login_check",
+              json = {
+                "token": "token",
+                 "refresh_token" : "refresh_token"
+                 }
+              )
 
+        # When
+        hl = HolaLuz()
+        # Then
+        self.assertEqual(hl.token, "token")
+
+    @responses.activate
+    def test_holaluz_failed_login(self):
+        responses.post(
+            "https://core.holaluz.com/api/private/login_check",
+            status = 404
+              )
+
+        hl = HolaLuz()
         
-    #     self.assertIn()
-    def test(self):
-        self.assertEqual(5,5)
+        self.assertIsNone(hl.token)
+
 
 
 if __name__ == "__main__":
