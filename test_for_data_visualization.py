7956ef3db9c3e38d34bc2f7f417a82b4d5e13394
sofia <106708718+sofisofa@users.noreply.github.com>
2022-11-09 17:45:35 +0100

    Started building the visualization of data and tests

---
+++ test_for_data_visualization.py
@@ -0,0 +1,12 @@
+import pytest
+import data_visualization
+from mockito import when, mock, verify, unstub
+import json
+import builtins
+ 
+def test_make_bar_plot():
+    
+    
+        
+if __name__ == "__main__":
+        pytest.main([__file__])
